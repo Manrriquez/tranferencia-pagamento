beeb226ddf7b723ee67c799c7ae32f400ad4216c
Manrriquez <manrriquez.contato@gmail.com>
2021-07-23 22:01:50 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+ // import logo from './logo.svg';
 import './App.css';
+import img from './img/usuario1.jpg'
+import img2 from './img/img2/usuario2.jpg'
+import img3 from './img/img3/usuario3.jpg'
+import img4 from './img/img4/usuario4.png'
 
-function App() {
+
+function Usuarios() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <div className="Perfil">
+        <img className="img" src={img} /> 
+      </div>
+         <div className="Usuarios">
+           <div className="dados">
+              <p className="name">Nome de Usuario: Lucas Souza</p>
+              <p className="id">ID: 1002</p>
+              <p className="username">Username: @LucasSouza12</p>
+              <div className="btn">
+                <button type="button">Pagar</button>
+              </div>
+           </div> 
+         </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default Usuarios;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Usuarios from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Usuarios />
   </React.StrictMode>,
   document.getElementById('root')
 );
